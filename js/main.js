d62aa690e47a5729f05f207965d135976167ae74
Даша <daria.tsenina@veeam.com>
2019-01-18 18:41:41 +0300

    доработки по п.1 ДЗ1

--- js/main.js
+++ js/main.js
@@ -1,31 +1,63 @@
-var createPhotosDescriptions = function () {
-	
-	var photosDescriptions = {
-		url: url,
-		likes: function getRandomNumber(min, max) {
-		},
-		comments: function createRandomComment () {
-			var randomComment = [
-			{ 	avatar: "",
-            	message: "",
-            	name: ""
-			},
-			]
-		}
+// получение рандомного числа
+var getRandomNumber = function (min, max) {
+	var randomNumber = min - 0.5 + Math.random() * (max - min + 1)
+	randomNumber = Math.round(randomNumber);
+	return randomNumber;
+};
 
-		};
+var messageArray = [
+'Всё отлично!', 
+'В целом всё неплохо. Но не всё.', 
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
-	return photosDescriptions;
-}
+var nameArray = [
+'Вадим',
+'Валентин',
+'Валерий',
+'Валерьян',
+'Варлам',
+'Варфоломей'
+]
 
-
-var createArrayOfDescriptions = function (numberOfElements) {
-	var ArrayOfDescriptions = [],
-
-	for (var i = 1; i <= numberOfElements; i++) {
-		createPhotosDescriptions;
+// создание массива комментариев
+var getComment = function () {
+	var comment = {
+		avatar: 'img/avatar-' + getRandomNumber(0, 6) + '.svg',
+		message: messageArray[getRandomNumber(0, 6)],
+		name: nameArray[getRandomNumber(0, 6)]
 	}
-}
+	return comment;
+	 
+};
+
+
+var createPhoto = function (index) {
+	return {
+		url: 'photos/' + index + '.jpg',
+		likes: getRandomNumber(0, 250),
+		comments: getComment()
+	};
+};
+
+var getPhotos = function (length) {
+	var photos = []
+	for (var i = 1; i <= length; i++) {
+		photos.push(createPhoto(i))
+	}
+	return photos;
+};
+
+var photos = getPhotos(25);
+
+console.log(photos)
+
+
+
+
 
 
 
