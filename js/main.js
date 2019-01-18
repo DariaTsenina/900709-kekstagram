8bdf1c9e538e57e664bdd9d84231d2d97da61af0
Даша <daria.tsenina@veeam.com>
2019-01-18 15:28:48 +0300

    Делает ДЗ https://up.htmlacademy.ru/javascript/16/tasks/13 пункт 1

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var createPhotosDescriptions = function () {
+	
+	var photosDescriptions = {
+		url: url,
+		likes: function getRandomNumber(min, max) {
+		},
+		comments: function createRandomComment () {
+			var randomComment = [
+			{ 	avatar: "",
+            	message: "",
+            	name: ""
+			},
+			]
+		}
+
+		};
+
+	return photosDescriptions;
+}
+
+
+var createArrayOfDescriptions = function (numberOfElements) {
+	var ArrayOfDescriptions = [],
+
+	for (var i = 1; i <= numberOfElements; i++) {
+		createPhotosDescriptions;
+	}
+}
+
+
+
+
+
