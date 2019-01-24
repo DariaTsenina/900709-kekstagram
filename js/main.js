c030af5e8620307fef0d74f1e7a2327c01104b93
Daria Tsenina <dtsenina@veeam.com>
2019-01-24 11:09:46 +0300

    Добавляет перемещение ползунка

--- js/main.js
+++ js/main.js
@@ -103,59 +103,17 @@ uploadCancel.addEventListener('click', function () {
   uploadFile.value = '';
 });
 
-// Переменные для наложения эффекта на изображение
+// Переменные для наложения эффекта на изображение и изменения масштаба
+var PIN_MAX_LEVEL = 100;
+var PIN_MIN_LEVEL = 0;
+
 var effects = document.querySelector('.effects');
 var preview = document.querySelector('.img-upload__preview > img');
 var effectLevelPin = document.querySelector('.effect-level__pin');
+var levelLine = document.querySelector('.effect-level__line');
 var effectLevelDepth = document.querySelector('.effect-level__depth');
 var effectLevelValue = document.querySelector('.effect-level__value');
 var effectLevel = document.querySelector('.effect-level');
-var pinScale = effectLevelValue.value;
-
-
-// Функция расчета интенсивности эффекта
-function effectCalc(min, max) {
-  return (max - min) * (pinScale / 100);
-}
-
-
-effects.addEventListener('change', function (event) {
-  var target = event.target;
-  var effect = target.value;
-  preview.className = 'effects__preview--' + effect;
-  preview.removeAttribute('style');
-  effectLevelPin.style.left = '100%';
-  effectLevelDepth.style.width = '100%';
-  pinScale = 100;
-  scaleValue.value = '100%';
-
-  if (effect === 'none') {
-    effectLevel.classList.add('hidden');
-  } else {
-    effectLevel.classList.remove('hidden');
-  }
-
-  // Изменение уровня насыщенности фильтра
-  effectLevelPin.addEventListener('mouseup', function () {
-    if (effect === 'chrome') {
-      var effectScale = effectCalc(0, 1);
-      preview.setAttribute('style', 'filter: grayscale(' + effectScale + ')');
-    } else if (effect === 'sepia') {
-      effectScale = effectCalc(0, 1);
-      preview.setAttribute('style', 'filter: sepia(' + effectScale + ')');
-    } else if (effect === 'marvin') {
-      effectScale = effectCalc(0, 100);
-      preview.setAttribute('style', 'filter: invert(' + effectScale + '%)');
-    } else if (effect === 'phobos') {
-      effectScale = effectCalc(0, 3);
-      preview.setAttribute('style', 'filter: blur(' + effectScale + 'px)');
-    } else {
-      effectScale = effectCalc(1, 3);
-      preview.setAttribute('style', 'filter: brightness(' + effectScale + ')');
-    }
-  });
-
-});
 
 // Масштаб изображения
 var scaleValue = document.querySelector('.scale__control--value');
@@ -194,4 +152,85 @@ buttonBigger.addEventListener('click', function () {
   return scaleValue;
 });
 
+
+// Перемещение ползунка
+
+effectLevelPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoord = event.clientX;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = startCoord - moveEvt.clientX;
+    startCoord = moveEvt.clientX;
+    var level = (effectLevelPin.offsetLeft - shift) / levelLine.clientWidth * 100;
+    if (level < PIN_MIN_LEVEL) {
+      level = PIN_MIN_LEVEL;
+    } else {
+      if (level > PIN_MAX_LEVEL) {
+        level = PIN_MAX_LEVEL;
+      }
+    }
+
+    effectLevelPin.style.left = level + '%';
+    effectLevelDepth.style.width = level + '%';
+    return level;
+
+  };
+
+  function onMouseUp(upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+// Функция расчета интенсивности эффекта
+var effectCalc = function (min, max) {
+  return min + (max - min) * (level / 100);
+};
+
+// Изменение фильтра
+effects.addEventListener('change', function (event) {
+  var target = event.target;
+  var effect = target.value;
+  preview.className = 'effects__preview--' + effect;
+  preview.removeAttribute('style');
+  effectLevelPin.style.left = '100%';
+  effectLevelDepth.style.width = '100%';
+  scaleValue.value = '100%';
+
+  if (effect === 'none') {
+    effectLevel.classList.add('hidden');
+  } else {
+    effectLevel.classList.remove('hidden');
+  }
+
+  // Изменение уровня насыщенности фильтра
+  effectLevelPin.addEventListener('mouseup', function () {
+    if (effect === 'chrome') {
+      var effectScale = effectCalc(0, 1);
+      preview.setAttribute('style', 'filter: grayscale(' + effectScale + ')');
+    } else if (effect === 'sepia') {
+      effectScale = effectCalc(0, 1);
+      preview.setAttribute('style', 'filter: sepia(' + effectScale + ')');
+    } else if (effect === 'marvin') {
+      effectScale = effectCalc(0, 100);
+      preview.setAttribute('style', 'filter: invert(' + effectScale + '%)');
+    } else if (effect === 'phobos') {
+      effectScale = effectCalc(0, 3);
+      preview.setAttribute('style', 'filter: blur(' + effectScale + 'px)');
+    } else {
+      effectScale = effectCalc(1, 3);
+      preview.setAttribute('style', 'filter: brightness(' + effectScale + ')');
+    }
+  });
+
+});
 
